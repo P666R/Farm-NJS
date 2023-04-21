9ea7bdc3386829d5570941cb31ea68f7cffc5eff
PRoy <sam469roger@gmail.com>
2023-04-21 23:25:59 +0530

    completed code

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
-// const fs = require("fs");
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-//////////////////////////////
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const cardsHtml = dataObj
+      .map((product) => replaceTemplate(tempCard, product))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    //  product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    //  api
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    //  not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>page not found !</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to request on port 8000");
+});
 
 /*
 //  blocking sync
@@ -38,35 +97,3 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 });
 console.log("will read file...");
 */
-
-////////////////////////////////////
-
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
-
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("overview");
-  } else if (pathName === "/product") {
-    res.end("product");
-  } else if (pathName === "/api") {
-    res.writeHead(200, {
-      "Content-type": "application/json",
-    });
-    res.end(data);
-  } else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-    });
-    res.end("<h1>page not found !</h1>");
-  }
-});
-
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to request on port 8000");
-});
